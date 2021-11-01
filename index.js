76712c59c54767f4334385a6f29828f89dfb1e81
Gurupriyan <gurupriyan98@gmail.com>
2021-11-01 13:41:29 +0000

    Completed the code challenge

--- index.js
+++ index.js
@@ -1,20 +1,47 @@
 //1
-function greatestOfTwo(){
-  
+function greatestOfTwo(a, b){
+  return (a>b) ? a : b;
 }
 
 //2
-function greatestOfThree(){
-  
+function greatestOfThree(a, b, c){
+  if ( a > b && a > c){
+      return a;
+  }else if (b > a && b > c){
+      return b;
+  }else {
+      return c;
+  }
 }
 
 //3
-function greatestOfFour(){
-  
+function greatestOfFour(a, b, c, d){
+  if ( a > b && a > c && a> d){
+      return a;
+  }else if (b > a && b > c && b > d){
+      return b;
+  }else if (c > a && c > b && c > d) {
+      return c;
+  }else {
+      return d;
+  }
 }
 
 
 //4
-function leastOfFour(){
-  
+function leastOfFour(a, b, c, d){
+  if ( a < b &&  a < c && a < d){
+      return a;
+  }else if (b < a && b < c && b < d){
+      return b;
+  }else if (c < a && c < b && c < d) {
+      return c;
+  }else {
+      return d;
+  }
 }
+
+// console.log(greatestOfTwo(4,5));
+// console.log(greatestOfThree(3, 6, 1));
+// console.log(greatestOfFour(8, 4, 1, 3));
+console.log(leastOfFour(9, 4, 5, 1));
